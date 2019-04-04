60fbdbb1a31ea510b4346532835622bb9fbda8cd
nurudeen adesina <access2abey@gmail.com>
2019-04-04 14:53:17 +0000

    feat: Complete the python process_data file

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,66 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    
+    '''
+    Args:
+        messages_filepath: Path to the messages data file
+        categories_filepath: Path to the categories data file
+    Returns:
+        df: Merged dataframe 
+    '''
+       
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages, categories, on='id')
+    
+    return df
 
 
 def clean_data(df):
-    pass
+    
+    '''
+    Args:
+        df: Merged dataframe
+    Returns:
+        df: Cleaned dataframe
+    '''
+    
+    categories = df['categories'].str.split(pat=';', expand=True)
+    row = categories.iloc[0]
+    category_colnames = row.apply(lambda x: x[:-2])
+    categories.columns = category_colnames
+    
+    # Convert category values to just numbers 0 or 1
+    for column in categories:
+        categories[column] = categories[column].str[-1]
+        categories[column] = categories[column].astype(int)
+    
+    # Drop the original categories column
+    df.drop('categories', axis=1, inplace=True)
+    
+    # Concatenate the original dataframe with the new categories dataframe
+    df = pd.concat([df, categories], axis=1)
+    df.drop_duplicates(subset='id', inplace=True)
+    
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    
+    '''
+    Save df into sqlite db
+    Args:
+        df: cleaned dataset
+        database_filename: database filename
+    Returns: None 
+        A SQLite database
+    '''
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('df_message_table', engine, index=False)
 
 
 def main():
